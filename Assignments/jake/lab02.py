beb281e16c233ca96f3634ccd14d8377d93f7a58
Jake Vinson <jacobvinson817@gmail.com>
2019-10-30 18:38:42 -0700

    Added first lab

---
+++ Assignments/jake/lab02.py
@@ -0,0 +1,18 @@
+#user greeting
+print ('Hello! Welcome to Mad Libs!')
+
+print ('Please answer the following questions:\n')
+
+Action = input('Name an Action (pass tense)... ')
+
+Noun1 = input ('Place a sound... ')
+
+monster = input ('Name a monster... ')
+
+
+adjective = input ('Name an adjective... ')
+
+monster1 = input ('Name a different monster... ')
+
+
+print(f'The dragon {Action} through the sky, as {Noun1} can be heard over the wind by what appears to be a {monster}. The dragon {Action} towards the sound to {adjective} from the {monster1}! The End! Fine! GO TO SLEEP NOW!')
