1fdebba4b60c9c101b6a4f314312a820e6a6ec56
Pete Jones <pwj2012@gmail.com>
2019-12-17 20:06:56 -0800

    added lab06
    it's timepage.com

--- Assignments/pete/javascript/lab06clock/script.js
+++ Assignments/pete/javascript/lab06clock/script.js
@@ -1,13 +1,52 @@
 window.onload = function() {
-    let main = document.querySelector('main')
-    let blockQuote = document.querySelector('blockquote')
-    let clockButton = document.querySelector('button.clock')
-    let stopwatchButton = document.querySelector('button.stopwatch')
-    let countdownButton = document.querySelectorAll('button.countdown')
+    let nav = document.querySelector('nav');
+    let main = document.querySelector('main.river');
+    // let mainClock = document.querySelector('main.clock')
+    let stopwatchDiv = document.querySelector('#stopwatch')
+    let allMain = document.querySelectorAll('main');
+    // allMain.style.display = 'none'
+    // mainClock.style.display = 'none'
+    let blockQuote = document.querySelector('blockquote');
+    let clockButton = document.querySelector('button.clock');
+    let stopwatchButton = document.querySelector('button.stopwatch');
+    let countdownButton = document.querySelectorAll('button.countdown');
+    let clockDiv = document.createElement('div');
+    // let clockDiv = document.querySelector('#clock')
+    //Clock
+    let stopwatchDiv = document.querySelector('#stopwatch')
     clockButton.onclick = function() {
-        main.classList.remove('river')
-        main.classList.add('clock')
-        blockQuote.style.display = 'none'
+        clockDiv.style.display = 'block';
+        main.classList.remove('river');
+        main.classList.remove('stopwatch');
+        main.classList.add('clock');
+        // allMain.style.display = 'none'
+        blockQuote.style.display = 'none';
+        stopwatchDiv.style.display = 'none';
+        // mainClock.style.display = 'block'
+        nav.classList.add('clock');
+        
+        clockDiv.classList.add('clock')
+        // main.innerHTML = ''
+        main.appendChild(clockDiv);
+        function clockFunction() {
+            setTimeout(clockFunction, 1000)
+            let date = new Date();
+            clockDiv.innerText = `It's ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
+        }
+        clockFunction()
+    }
+
+    //Stopwatch
+    stopwatchButton.onclick = function() {
+        // main.innerHTML = ''
+        clockDiv.style.display = 'none';
+        main.classList.remove('clock')
+        main.classList.remove('river');
+        main.classList.add('stopwatch');
+        blockQuote.style.display = 'none';
+        stopwatchDiv.style.display = 'block';
+
     }
 
 }
