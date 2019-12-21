143b8fb175f9721121bfaa6069b130cff0ff412e
Pete Jones <pwj2012@gmail.com>
2019-12-21 11:07:57 -0800

    more work on bouncing-ball
    And the adventures of blocky

---
+++ Assignments/pete/javascript/lab08bouncing-ball/jumper/script.js
@@ -0,0 +1,129 @@
+let width = 1000;
+let height = 1000;
+let cnv = document.querySelector('canvas')
+let ctx = cnv.getContext('2d');
+let ground
+
+ctx.fillStyle = 'brown'
+ctx.fillRect(25, 750, 250, 50)
+ctx.fillRect(375, 750, 250, 50)
+ctx.fillRect(725, 750, 250, 50)
+
+
+let blocky = {
+    color: 'green',
+    height: 75,
+    width: 35,
+    px: 75,
+    py: 500,
+    vx: 0,
+    vy: 0,
+    ay: 0.1,
+    ground: '',
+}
+
+let rect1 = {
+    color: 'red',
+    px: 25,
+    py: 750,
+    width: 250,
+    height: 50,
+}
+
+let rect2 = {
+    color: 'yellow',
+    px: 375,
+    py: 750,
+    width: 250,
+    height: 50,
+}
+
+let rect3 = {
+    color: 'blue',
+    px: 725,
+    py: 750,
+    width: 250,
+    height: 50,
+}
+
+let rectArr = [rect1, rect2, rect3]
+
+function mainLoop() {
+    ctx.clearRect(0, 0, width, height)
+    for (let i=0; i<rectArr.length; i++) {
+        ctx.fillStyle = rectArr[i].color
+        ctx.fillRect(rectArr[i].px, rectArr[i].py, rectArr[i].width, rectArr[i].height)
+        
+        //text
+        ctx.fillStyle = 'black';
+        ctx.font = "3rem Arial";
+        ctx.fillText("The Adventures of Blocky", 225, 315)
+
+        //update blocky
+        blocky.px += blocky.vx
+        blocky.py += blocky.vy
+        blocky.vy += blocky.ay//grav
+
+        //draw blocky
+        ctx.fillStyle = blocky.color
+        ctx.fillRect(blocky.px, blocky.py, blocky.width, blocky.height);
+
+        //check collision
+        if (blocky.ground === true) {
+            if (blocky.px + blocky.width < rectArr[i].px//blocky.px check1
+                && blocky.px > rectArr[i].px + rectArr[i].width//blocky.px check2
+            ) {
+                blocky.ground = false;
+            }
+        }
+
+        // else {
+        //     blocky.ground = true;
+        // }
+
+        if (blocky.ground) {
+            blocky.ay = 0;
+            // blocky.ground = false;
+            continue;
+        }
+
+        else if (
+            (blocky.py + blocky.height > rectArr[i].py//blocky.py check1
+                && blocky.py < rectArr[i].py + rectArr[i].height)//blocky.py check2
+            && (blocky.px + blocky.width > rectArr[i].px//blocky.px check1
+                && blocky.px < rectArr[i].px + rectArr[i].width)//blocky.px check2
+            ) {
+            blocky.py = rectArr[i].py - blocky.height;
+            blocky.vy = 0;
+            blocky.ay = 0;
+            blocky.ground = true;
+        }
+        // else {
+        //     blocky.ay = 0.1;
+        //     blocky.ground = false;
+        // }
+    }
+    
+    window.requestAnimationFrame(mainLoop)
+}
+window.requestAnimationFrame(mainLoop)
+
+
+document.addEventListener('keydown', function(e) {
+    if (e.which === 37 || e.which === 65) {
+        blocky.vx = -1;
+    }
+    if (e.which === 39 || e.which === 68) {
+        blocky.vx = 1;
+    }
+    if (e.which === 32  && blocky.ground) {
+        blocky.vy = -5;
+        blocky.ay = 0.1;
+        blocky.ground = false;
+    }
+})
+document.addEventListener('keyup', function(e) {
+    if (e.which === 37 || e.which === 39 || e.which === 65 || e.which === 68){
+        blocky.vx = 0;
+    }
+})
