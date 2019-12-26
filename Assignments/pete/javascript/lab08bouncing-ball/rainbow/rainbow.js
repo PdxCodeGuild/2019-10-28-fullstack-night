ec610c375e973d523f82a54b45f76ec5c16dd2e0
Pete Jones <pwj2012@gmail.com>
2019-12-26 12:58:15 -0800

    rainbow balls
    bouncing rainbow balls

--- Assignments/pete/javascript/lab08bouncing-ball/rainbow/rainbow.js
+++ Assignments/pete/javascript/lab08bouncing-ball/rainbow/rainbow.js
@@ -3,12 +3,43 @@ let ctx = cnv.getContext('2d');
 let width = 1920;
 let height = 1080;
 let gravity = {
-    ax: 0.5,
-    ay: 0.
+    ax: 0.1,
+    ay: 0.4,
 }
-// let ax = 0
-// let ay = 0.5
-let r = 2
+accDir = {
+    x: '',
+    y: '',
+}
+
+
+let gravObjs = [
+    obj01 = {ax:  0.0, ay:  0.5},
+    obj02 = {ax: -0.1, ay:  0.4},
+    obj03 = {ax: -0.2, ay:  0.3},
+    obj04 = {ax: -0.3, ay:  0.2},
+    obj05 = {ax: -0.4, ay:  0.1},
+
+    obj06 = {ax: -0.5, ay:  0.0},
+    obj07 = {ax: -0.4, ay: -0.1},
+    obj08 = {ax: -0.3, ay: -0.2},
+    obj09 = {ax: -0.2, ay: -0.3},
+    obj10 = {ax: -0.1, ay: -0.4},
+
+    obj11 = {ax:  0.0, ay: -0.5},
+    obj12 = {ax:  0.1, ay: -0.4},
+    obj13 = {ax:  0.2, ay: -0.3},
+    obj14 = {ax:  0.3, ay: -0.2},
+    obj15 = {ax:  0.4, ay: -0.1},
+
+    obj16 = {ax:  0.5, ay:  0.0},
+    obj17 = {ax:  0.4, ay:  0.1},
+    obj18 = {ax:  0.3, ay:  0.2},
+    obj19 = {ax:  0.2, ay:  0.3},
+    obj20 = {ax:  0.1, ay:  0.4},
+]
+// let ax = 0.1
+// let ay = 0.4
+let r = 1
 
 let rainbowBalls = [
     red = {
@@ -72,13 +103,25 @@ let rainbowBalls = [
 function borderCollision(rainbowBalls) {
     for (let i=0; i<rainbowBalls.length; i++) {
         if (rainbowBalls[i].px <= rainbowBalls[i].radius || rainbowBalls[i].px >= width - rainbowBalls[i].radius) {
-            rainbowBalls[i].vx *= -0.99
-            rainbowBalls[i].vy *= 0.99
+            //friction
+            // rainbowBalls[i].vx *= -0.99
+            // rainbowBalls[i].vy *= 0.99
+            //no friction
+            rainbowBalls[i].vx *= -1
+
+
+
         }
         if (rainbowBalls[i].py <= rainbowBalls[i].radius || rainbowBalls[i].py >= height - rainbowBalls[i].radius) {
-            rainbowBalls[i].vy *= -0.99
-            rainbowBalls[i].vx *= 0.99
+            //friction
+            // rainbowBalls[i].vy *= -0.99
+            // rainbowBalls[i].vx *= 0.99
+            //no friction
+            rainbowBalls[i].vy *= -1
         } 
+        rainbowBalls[i].px += rainbowBalls[i].vx
+        rainbowBalls[i].py += rainbowBalls[i].vy
+
     }
 }
 
@@ -129,4 +172,59 @@ function changeGrav () {
         gravity.ay = 0.5
     }
 }
-changeGrav()
+
+function changeGrav2 () {
+    setTimeout(changeGrav2, 1000)
+
+    if ((gravity.ax <= 0 && gravity.ax > -0.5) && (gravity.ay > 0 && gravity.ay <= 0.5)) {
+        accDir.x = 'left';
+        accDir.y = 'up';
+    } else if ((gravity.ax >= -0.5 && gravity.ax < 0) && (gravity.ay <= 0 && gravity.ay > -0.5)) {
+        accDir.x = 'right';
+        accDir.y = 'up';
+    } else if ((gravity.ax >= 0 && gravity.ax <0.5) && (gravity.ay >= -0.5 &&gravity.ay < 0)) {
+        accDir.x = 'right';
+        accDir.y = 'down';
+    } else if ((gravity.ax <= 0.5 && gravity.ax > 0) && (gravity.ay > 0 && gravity.ay < 0.5)) {
+        accDir.x = 'left';
+        accDir.y = 'down'
+    }
+    
+    if (accDir.x === 'left') {
+        gravity.ax -= .1;
+    } else if (accDir.x === 'right') {
+        gravity.ax += .1
+    }
+
+    if (accDir.y === 'up') {
+        gravity.ay -= .1;
+    } else if (accDir.y === 'down') {
+        gravity.ay += .1;
+    }
+    console.log(gravity)
+    console.log(accDir)
+}
+
+function changeGrav3 () {
+    setTimeout(changeGrav3, 5000)
+
+    for (let i=0; i<gravObjs.length; i++) {
+        if (gravity.ax === gravObjs[i].ax && gravity.ay === gravObjs[i].ay) {
+            gravity.ax = gravObjs[(i + 1) % gravObjs.length].ax
+            gravity.ay = gravObjs[(i + 1) % gravObjs.length].ay
+            break
+        }
+    }
+}
+
+function getBigger() {
+    setTimeout(getBigger, 5000)
+
+    for (let i=0; i<rainbowBalls.length; i++) {
+        rainbowBalls[i].radius += 1
+    }
+
+}
+
+changeGrav3()
+getBigger()
