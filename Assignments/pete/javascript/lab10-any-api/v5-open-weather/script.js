ab87ddc0500081d0e2acc869a77878d4715a8bf3
Pete Jones <pwj2012@gmail.com>
2020-01-11 21:09:57 -0800

    open weather vue

--- Assignments/pete/javascript/lab10-any-api/v5-open-weather/script.js
+++ Assignments/pete/javascript/lab10-any-api/v5-open-weather/script.js
@@ -1,12 +1,11 @@
-function getWeather() {
-    return [{main: 'hey'},'b','c']
-}
+// function addId(forecastsNoId)
 let forecastDiv = {
     props: ['forecast'],
-    template: `<div class="outer-container-div">
-        <img src='http://openweathermap.org/img/wn/{{forecast.icon}@2x.png'>
+    template: `<div class="outer-container">
+        <img :src="'http://openweathermap.org/img/wn/' + forecast.weather[0].icon + '@2x.png'">
 
-        <div class="inner-container-div">
+        <div class="inner-container">
+            <div class="time">{{forecast.dt}}</div>
             <div class="temp">{{forecast.main.temp}}</div>
             <div class="feels-like">{{ forecast.main.feels_like}}</div>
             <div class="humidity">{{ forecast.main.humidity}}</div>
@@ -14,11 +13,11 @@ let forecastDiv = {
             <div class="wind">{{forecast.wind.speed}}</div>
         </div>
     </div>`,
-    data: () => ({
-        forecast: getWeather(),
-    }),
+    // data: () => ({
+    //     forecast: getWeather(),
+    // }),
     // data: {
-    //     forecasts: ['1','2','3']
+    //     forecasts: []
     // }
 }
 
@@ -26,11 +25,8 @@ let forecastDiv = {
 var vm = new Vue({
     el: '#vm',
     data: {
-        city: 'seattle',
-        forecasts: [{
-            main: 'main',
-
-        }],
+        city: '',
+        forecasts: [],
     },
     methods: {
 
@@ -39,26 +35,32 @@ var vm = new Vue({
         forecastDiv,
     }
 })
-
-//axios stuff
 let apiKey = "2a3b6c8a4967eb6fc1f41df5bbf6999b"
 let url = `https://api.openweathermap.org/data/2.5/weather?q=${vm.city}&units=imperial&APPID=${apiKey}`
-axios.get(url)
-.then(function(response) {
-    console.log('current weather', response)
-    let data = response.data
-    let name = data.name
-    // nameDiv.innerText = `${name} Now:`
-    // weather(data, todayArr)
-})
 let url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${vm.city}&mode=JSON&units=imperial&APPID=${apiKey}`
-axios.get(url2)
-.then(function(response) {
-    console.log('5 day', response)
-    // console.log(2, response, tomorrowArr)
-    let data = response.data
-    let city = data.city
-    let list = data.list
-    let tomorrow = list[8]
-    // weather(tomorrow, tomorrowArr)
+
+let input = document.querySelector('input')
+input.addEventListener('keydown', function(e) {
+    if (e.which === 13) {
+        console.log('were in!')
+//axios stuff
+        axios.get(url)
+        .then(function(response) {
+            console.log('current weather', response)
+            let forecast = response.data
+            forecast.id = 0
+            vm.forecasts.push(forecast)
+        })
+        axios.get(url2)
+        .then(function(response) {
+            console.log('5 day', response)
+            let list = response.data.list
+            for (let i=0; i<list.length; i++) {
+                let forecast = list[i]
+                forecast.id = i + 1
+                vm.forecasts.push(forecast)
+
+            }
+        })
+    }
 })
