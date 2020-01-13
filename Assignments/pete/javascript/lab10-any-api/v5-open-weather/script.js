e24855c3ce18ddfc3017d26fca74d39256f9b777
Pete Jones <pwj2012@gmail.com>
2020-01-13 09:58:16 -0800

    open weather vue
    vue, open weather

--- Assignments/pete/javascript/lab10-any-api/v5-open-weather/script.js
+++ Assignments/pete/javascript/lab10-any-api/v5-open-weather/script.js
@@ -1,11 +1,12 @@
-// function addId(forecastsNoId)
+//vue component forecastDiv
 let forecastDiv = {
     props: ['forecast'],
     template: `<div class="outer-container">
         <img :src="'http://openweathermap.org/img/wn/' + forecast.weather[0].icon + '@2x.png'">
 
         <div class="inner-container">
-            <div class="time">{{forecast.dt}}</div>
+            <div class="datetime">{{forecast.dt}}</div>
+            <div class="dt-text">{{forecast.dt_txt.toLocaleString()}}</div>
             <div class="temp">{{forecast.main.temp}}</div>
             <div class="feels-like">{{ forecast.main.feels_like}}</div>
             <div class="humidity">{{ forecast.main.humidity}}</div>
@@ -25,8 +26,10 @@ let forecastDiv = {
 var vm = new Vue({
     el: '#vm',
     data: {
+        search: '',
         city: '',
         forecasts: [],
+        timeZone: '',
     },
     methods: {
 
@@ -35,21 +38,28 @@ var vm = new Vue({
         forecastDiv,
     }
 })
-let apiKey = "2a3b6c8a4967eb6fc1f41df5bbf6999b"
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${vm.city}&units=imperial&APPID=${apiKey}`
-let url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${vm.city}&mode=JSON&units=imperial&APPID=${apiKey}`
+let cityName = document.querySelector('#city-name')
 
 let input = document.querySelector('input')
 input.addEventListener('keydown', function(e) {
     if (e.which === 13) {
         console.log('were in!')
-//axios stuff
+        //clear vm.forecasts
+        vm.forecasts = []
+        //axios stuff
+        let apiKey = "2a3b6c8a4967eb6fc1f41df5bbf6999b"
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${vm.search}&units=imperial&APPID=${apiKey}`
+        let url2 = `https://api.openweathermap.org/data/2.5/forecast?q=${vm.search}&mode=JSON&units=imperial&APPID=${apiKey}`
         axios.get(url)
         .then(function(response) {
             console.log('current weather', response)
             let forecast = response.data
+            vm.city = forecast.name
+            cityName.innerText = `${vm.city} Current:`
+            vm.timeZone = forecast.timezone
             forecast.id = 0
             vm.forecasts.push(forecast)
+        
         })
         axios.get(url2)
         .then(function(response) {
