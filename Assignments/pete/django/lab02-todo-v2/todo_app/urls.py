bc26d6a9167a7ac80abc3720748c695f33d9b277
Pete Jones <pwj2012@gmail.com>
2020-01-25 19:21:06 -0800

    todov2

--- Assignments/pete/django/lab02-todo-v2/todo_app/urls.py
+++ Assignments/pete/django/lab02-todo-v2/todo_app/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'todo_app'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('complete/', views.complete, name='complete'),
 ]
--- Assignments/pete/django/lab02-todo-v2/todo_app/views.py
+++ Assignments/pete/django/lab02-todo-v2/todo_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 
 from .models import TodoItem
 
@@ -8,3 +9,9 @@ def index(request):
         TodoItem(text=request.POST['text']).save()
         HttpResponseRedirect(reverse('todo_app:index'))
     return render(request, 'todo_app/index.html', {'todos': TodoItem.objects.all()})
+
+def complete(request):
+    if request.method == "POST":
+        todo = TodoItem.objects.get(pk=int(request.POST['name']))
+        todo.completed_bool = True
+        todo.completed_date = timezone.now()
