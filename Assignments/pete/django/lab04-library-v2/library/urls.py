a8dee6a8588809b91ee7706b12618e606cd89841
Pete Jones <pwj2012@gmail.com>
2020-01-29 10:06:19 -0800

    django

--- Assignments/pete/django/lab04-library-v2/library/urls.py
+++ Assignments/pete/django/lab04-library-v2/library/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('checkout-history/<str:title>/', views.checkout_history, name='checkout_history'),
     path('donate/', views.donate, name='donate'),
     path('checkout/<str:title>', views.checkout_book, name='checkout'),
-    path('return/', views.return_book, name='return'),
+    path('return/<str:title>', views.return_book, name='return'),
+    path('choose-author/<str:author>', views.choose_author, name='choose_author'),
+    path('new-author/', views.new_author, name='new_author'),
+    path('old-author/<str:author>', views.old_author, name='old_author'),
 ]
--- Assignments/pete/django/lab04-library-v2/library/views.py
+++ Assignments/pete/django/lab04-library-v2/library/views.py
@@ -11,7 +11,7 @@ def checkout_history(request, title):
     return render(request, 'library/checkout-history.html', {'checkouts': book.checkouts.all(), 'book': book})
 
 def donate(request):
-    return render(request, 'library/donate.html')
+    return render(request, 'library/donate.html', {'authors': Author.objects.all()})
 
 def checkout_book(request, title):
     if request.method == 'POST':
@@ -19,8 +19,32 @@ def checkout_book(request, title):
         book.available = False
         user = request.POST['user']
         Checkout(checked_out=True, user=user, book=book).save()
-        return HttpResponseRedirect(reverse('library:index'))
-    return render(request, 'library/confirm.html', {})
+        book.save()
+        return HttpResponseRedirect(reverse('library:checkout_history', args=[title]))
+    # return render(request, 'library/confirm.html', {})
 
 def return_book(request, title):
-    pass
+    if request.method == 'POST':
+        book = Book.objects.get(title=title)
+        book.available = True
+        user = request.POST['user']
+        Checkout(checked_out=False, user=user, book=book).save()
+        book.save()
+        return HttpResponseRedirect(reverse('library:checkout_history', args=[title]))
+    # return render(request, 'library/checkokut-history.html', {'checkouts': book.checkouts.all(), 'book': book})
+
+def choose_author(request, author):
+    if request.method == 'POST':
+        if request.POST['author'] == 'new':
+            #go about adding new author
+            # return render(request, 'library/new-author.html')
+            new_author(request)
+        else:
+            old_author(request, author)
+            # return render(request, 'library/old-author.html', {'author': Author.objects.get(name=request.POST['author'])})
+
+def new_author(request):
+    return render(request, 'library/new-author.html')
+
+def old_author(request, author):
+    return render(request, 'library/old-author.html', {'author': Author.objects.get(name=author)})
