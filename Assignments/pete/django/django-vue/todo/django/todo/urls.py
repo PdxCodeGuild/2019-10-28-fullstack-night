c9e89b02d6a9af5b12c04a0ccf354d205ab848c1
Pete Jones <pwj2012@gmail.com>
2020-01-30 21:14:32 -0800

    django

--- Assignments/pete/django/django-vue/todo/django/todo/urls.py
+++ Assignments/pete/django/django-vue/todo/django/todo/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'todo'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('new-task/', views.new_task, name='new_task'),
+    path('complete-task/', views.complete_task, name='complete_task'),
 ]
--- Assignments/pete/django/django-vue/todo/django/todo/views.py
+++ Assignments/pete/django/django-vue/todo/django/todo/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.shortcuts import render, reverse
+from django.http import JsonResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+
+import json
 
 from .models import Task
+
 
 def index(request):
     tasks = Task.objects.all()
@@ -10,6 +15,37 @@ def index(request):
         'text': task.text,
         'created': task.created,
         'completed': task.completed,
-        'completed-bool': task.completed_bool,
+        'completedBool': task.completed_bool,
     } for task in tasks]
     return JsonResponse({'tasks': task_list})
+
+@csrf_exempt
+def new_task(request):
+    if request.method == 'POST':
+        text = json.loads(request.body)['text']
+        Task(text=text).save()
+        task_list = get_tasks()
+        return JsonResponse({'tasks': task_list})
+
+@csrf_exempt
+def complete_task(request):
+    if request.method == 'POST':
+        pk = json.loads(request.body)['id']
+        task = Task.objects.get(id=pk)
+        task.completed_bool = True
+        task.completed = timezone.now()
+        task.save()
+        task_list = get_tasks()
+        return JsonResponse({'tasks': task_list})
+
+
+def get_tasks():
+    tasks = Task.objects.all()
+    task_list = [{
+        'id': task.id,
+        'text': task.text,
+        'created': task.created,
+        'completed': task.completed,
+        'completedBool': task.completed_bool,
+    } for task in tasks]
+    return task_list
