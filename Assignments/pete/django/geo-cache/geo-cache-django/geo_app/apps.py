753cc68137f2768541e12caaad27965174398be8
Pete Jones <pwj2012@gmail.com>
2020-02-04 19:36:21 -0800

    geo_cache

---
+++ Assignments/pete/django/geo-cache/geo-cache-django/geo_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GeoAppConfig(AppConfig):
+    name = 'geo_app'
---
+++ Assignments/pete/django/geo-cache/geo-cache-django/geo_app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class GeoCache(models.Model):
+    lat = models.FloatField()
+    lng = models.FloatField()
+    name = models.CharField(max_length=140)
+
+    def __str__(self):
+        return f"Name: {self.name}; Latitude: {self.lat}; Longitude: {self.lng}"
