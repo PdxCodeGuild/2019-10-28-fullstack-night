baeae2ba8045685a9fc14f45da32099d2eb966a7
Pete Jones <pwj2012@gmail.com>
2020-02-07 17:31:14 -0800

    in case what I do next breaks everything

--- Assignments/pete/capstone/prototype/nutrition_tracker/tracker/admin.py
+++ Assignments/pete/capstone/prototype/nutrition_tracker/tracker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Meal, DiaryDay, DiaryEntry
+from .models import Meal, DiaryDay, DiaryEntry, UserProfile
 
 admin.site.register(Meal)
 admin.site.register(DiaryDay)
 admin.site.register(DiaryEntry)
+admin.site.register(UserProfile)
