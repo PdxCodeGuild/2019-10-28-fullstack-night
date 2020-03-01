4ccb97e10dabca7ea04fa35e033af83da6929356
Pete Jones <pwj2012@gmail.com>
2020-02-29 18:58:35 -0800

    capstone

---
+++ Assignments/pete/capstone/canvas/test01/script.js
@@ -0,0 +1,21 @@
+let ctx = document.querySelector('canvas').getContext('2d');
+let w = 500
+let h = 300
+
+var app = new Vue({
+    el: '#app',
+    data: {
+
+        goalKcal: '',
+        goalFat: '',
+        goalCarb: '',
+        goalProtein: '',
+
+        Kcal: '',
+        Fat: '',
+        Carb: '',
+        Protein: '',
+
+    },
+})
+
