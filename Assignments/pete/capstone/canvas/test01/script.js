847305697f6980e162ae5667714b5d54a42e07da
Pete Jones <pwj2012@gmail.com>
2020-03-01 11:58:31 -0800

    capstone

--- Assignments/pete/capstone/canvas/test01/script.js
+++ Assignments/pete/capstone/canvas/test01/script.js
@@ -1,6 +1,6 @@
-let ctx = document.querySelector('canvas').getContext('2d');
-let w = 500
-let h = 300
+// let ctx = document.querySelector('canvas').getContext('2d');
+// let w = 500;
+// let h = 300;
 
 var app = new Vue({
     el: '#app',
@@ -11,11 +11,36 @@ var app = new Vue({
         goalCarb: '',
         goalProtein: '',
 
-        Kcal: '',
-        Fat: '',
-        Carb: '',
-        Protein: '',
+        kcal: '',
+        fat: '',
+        carb: '',
+        protein: '',
 
+        progressObj: {
+            'goalKcal': this.goalKcal,
+            'goalFat': this.goalFat,
+            'goalCarb': this.goalCarb,
+            'goalProtein': this.goalProtein,
+    
+            'kcal': this.kcal,
+            'fat': this.fat,
+            'carb': this.carb,
+            'protein': this.protein,
+        },
+
+        w: 500,
+        h: 300,
     },
-})
+    directives: {
+        loadCanvas: function(el, binding) {
+            console.log(el)
+            console.log(binding)
+            let ctx = el.getContext('2d');
+            // ctx.clearRect(0, 0, this.w, this.h);
+            ctx.fillStyle = 'hotpink';
+            ctx.fillRect(0, 0, this.w, this.h);
+            console.log(this.w, this.h);
+        }
+    }
+});
 
