053e398e57474d3971887f312e629d1a72507de9
ach1984 <57276519+ach1984@users.noreply.github.com>
2020-03-28 12:30:33 -0700

    Add files via upload

---
+++ Assignments/andrew/capstone/presentation/quotes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Stock
+
+admin.site.register(Stock)
+
+
---
+++ Assignments/andrew/capstone/presentation/quotes/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Stock
+
+class StockForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = ["ticker"]
+
+
